c619e71716b897d4b0bc4c0607c2e80d972b7946
pavan-chilukuri-nh104834 <pavan.chilukuri@hertz.com>
2022-03-07 15:52:10 -0600

    Save pokemon functionality

--- index.js
+++ index.js
@@ -1,30 +1,57 @@
 const thisForm = document.getElementById("myForm");
 const cardBack = document.querySelector(".flip-card-back");
+const carousel = document.querySelector(".carousel");
+const carouselContainer = document.querySelector(".carouselContainer");
+const card = document.querySelector(".flip-card");
+
 const pokemonEl = document.createElement("div");
-const x = document.querySelector(".flip-card");
+const saveBtn = document.createElement("div");
+const slide = document.createElement("div");
+
+const pokemons = [];
+const searchedPokemons = [];
+
+const geyPokemonByNameOrId = async (searchValue) => {
+  return await fetch(`https://pokeapi.co/api/v2/pokemon/${searchValue}`);
+};
+
+function getUniqueListBy(arr, key) {
+  return [...new Map(arr.map((item) => [item[key], item])).values()];
+}
 
 thisForm.addEventListener("submit", async function (e) {
   e.preventDefault();
-
-  x.style.display = "block";
+  card.style.display = "block";
 
   const searchValue = document.getElementById("search").value;
 
-  const response = await fetch(
-    `https://pokeapi.co/api/v2/pokemon/${searchValue}`
-  );
+  const response = await geyPokemonByNameOrId(searchValue);
 
   const result = await response.json();
 
+  localStorage.setItem("searchedPokemon", JSON.stringify(result));
+
+  searchedPokemons.push(result);
+
+  const uniqueSearchedPokemons = getUniqueListBy(searchedPokemons, "id");
+
+  localStorage.setItem(
+    "uniqueSearchedPokemons",
+    JSON.stringify(uniqueSearchedPokemons)
+  );
+
   document.getElementById("pokemonImage").src = result.sprites.front_default;
+
+  const searchedPokemonImg = result.sprites.front_default;
+  const searchedPokemonId = result.id;
 
   pokemonEl.innerHTML = `
   
         <div class="pokemonName">${result.forms[0].name}</div>
-  <div class="stats">
-  <div class="title">Stats</div>
+            <div class="stats">
+    <div class="title">Stats</div>
       ${result.stats
-        .slice(0, 4)
+        .slice(0, 3)
         .map((stat) => {
           return `<div>${stat.stat.name}: ${stat.base_stat}</div>`;
         })
@@ -34,7 +61,7 @@ thisForm.addEventListener("submit", async function (e) {
     <div class="abilities">
     <div class="title">Abilities</div>
     ${result.abilities
-      .slice(0, 4)
+      .slice(0, 3)
       .map((ability) => {
         return `<div>${ability.ability.name}</div>`;
       })
@@ -44,7 +71,81 @@ thisForm.addEventListener("submit", async function (e) {
     <div class="moves">
     <div class="title">Moves</div>
     ${result.moves
-      .slice(0, 4)
+      .slice(0, 3)
+      .map((move) => {
+        return `<div>${move.move.name}</div>`;
+      })
+      .join("")}
+    <div>
+  `;
+
+  saveBtn.innerHTML = `
+   <div>
+   <button style="margin-top: 24px" onclick="saveToCarousel('${searchedPokemonImg}', '${searchedPokemonId}')">Save</button>
+   </div>
+   `;
+
+  cardBack.appendChild(pokemonEl);
+  card.appendChild(saveBtn);
+});
+
+const saveToCarousel = (img, id) => {
+  carouselContainer.style.display = "block";
+  pokemons.push({ id, img });
+
+  const uniquePokemons = getUniqueListBy(pokemons, "id");
+
+  localStorage.setItem("savedPokemons", uniquePokemons);
+
+  slide.innerHTML = `
+  ${uniquePokemons
+    .map((pokemon) => {
+      return `<img class="carouselImg" onclick="displayOnDeck('${pokemon.id}')" src="${pokemon.img}"/>`;
+    })
+    .join("")}
+    `;
+
+  carousel.appendChild(slide);
+};
+
+const displayOnDeck = (pokemonId) => {
+  const pokemons = localStorage.getItem("uniqueSearchedPokemons");
+  const parsedPokemons = JSON.parse(pokemons);
+
+  const selectedPokemon = parsedPokemons.find(
+    (val) => val.id === JSON.parse(pokemonId)
+  );
+
+  document.getElementById("pokemonImage").src =
+    selectedPokemon.sprites.front_default;
+
+  pokemonEl.innerHTML = `
+  
+        <div class="pokemonName">${selectedPokemon.forms[0].name}</div>
+            <div class="stats">
+    <div class="title">Stats</div>
+      ${selectedPokemon.stats
+        .slice(0, 3)
+        .map((stat) => {
+          return `<div>${stat.stat.name}: ${stat.base_stat}</div>`;
+        })
+        .join("")}
+    </div>
+
+    <div class="abilities">
+    <div class="title">Abilities</div>
+    ${selectedPokemon.abilities
+      .slice(0, 3)
+      .map((ability) => {
+        return `<div>${ability.ability.name}</div>`;
+      })
+      .join("")}
+    <div>
+
+    <div class="moves">
+    <div class="title">Moves</div>
+    ${selectedPokemon.moves
+      .slice(0, 3)
       .map((move) => {
         return `<div>${move.move.name}</div>`;
       })
@@ -53,43 +154,4 @@ thisForm.addEventListener("submit", async function (e) {
   `;
 
   cardBack.appendChild(pokemonEl);
-});
-
-const flipCard = () => {
-  console.log("flipped card clicked");
 };
-
-// var slideIndex = 1;
-// showSlides(slideIndex);
-
-// function plusSlides(n) {
-//   showSlides((slideIndex += n));
-// }
-
-// function currentSlide(n) {
-//   showSlides((slideIndex = n));
-// }
-
-// function showSlides(n) {
-//   var i;
-//   var slides = document.getElementsByClassName("mySlides");
-//   var dots = document.getElementsByClassName("dot");
-//   if (n > slides.length) {
-//     slideIndex = 1;
-//   }
-//   if (n < 1) {
-//     slideIndex = slides.length;
-//   }
-//   for (i = 0; i < slides.length; i++) {
-//     slides[i].style.display = "none";
-//   }
-//   for (i = 0; i < dots.length; i++) {
-//     dots[i].className = dots[i].className.replace(" active", "");
-//   }
-//   slides[slideIndex - 1].style.display = "block";
-//   dots[slideIndex - 1].className += " active";
-// }
-
-// const saveToCarousel = () => {
-//   console.log("save to carousel");
-// };
